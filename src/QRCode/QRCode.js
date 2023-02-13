81d673552f115333ed4fd68f5b86fe0bb92a7242
abdulrehman202 <abdulrehman202@users.noreply.github.com>
2023-02-13 19:10:27 +0500

    token generation successful

--- src/QRCode/QRCode.js
+++ src/QRCode/QRCode.js
@@ -1,73 +1,153 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import {QRCodeSVG} from 'qrcode.react';
 import './QRCode.css'
 import ClipLoader from 'react-spinners/ClipLoader';
 import AgoraRTC from "agora-rtc-sdk-ng"
-import { AgoraVideoPlayer } from "agora-rtc-react";
 import AgoraUIKit from 'agora-react-uikit';
+import axios from 'axios';
 
-function QRcode() 
+class QRcode extends React.Component 
 {
-  const [data, setData] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [participantJoined, setparticipantJoined] = useState(false);
-  const [remoteTrack, setremoteTrack] = useState(null);
+  //This is the main component that renders QR COde and video view
+    constructor(props)
+    {
+      //initialization of all the attributes
+      super(props);
+      this.agoraEngine = AgoraRTC.createClient({ mode: "live", codec: "vp8" });
+      this.agoraEngine.setClientRole('audience');
+      this.APP_ID = 'eb89e77a6cae45a39d1c547598be879e';
 
-  const CHANNEL_NAME = 'rtc8108';
-  const TOKEN = '007eJxTYHA6GKXduFVt+ev3Ensl586c+dTtq9E9pmf7JnfMfd/38McTBYbUJAvLVHPzRLPkxFQT00RjyxTDZFMTc1NLi6RUC3PLVO5jz5MbAhkZZPW6GBkZIBDEZ2coKkm2MDSwYGAAAK7cIvg='; 
-  const APP_ID = 'eb89e77a6cae45a39d1c547598be879e';
-  const UID = 0;
-  const rtcProps = {
-    appId: APP_ID,
-    channel: CHANNEL_NAME, // your agora channel
-    token: TOKEN, // use null or skip if using app in testing mode
-    role: 'audience',
-    enableScreensharing: true
-  };
-  const agoraEngine = AgoraRTC.createClient({ mode: "live", codec: "vp8" });
-  agoraEngine.setClientRole('audience');
+      this.agoraEngine.on('user-joined',async (user)=>
+  {
+    //Triggers when a user joins
 
-    React.useEffect( () =>{
+    //The web-end removes its audio & vide streams
+    await this.agoraEngine.unpublish(this.state.agoraEngine.localTracks);
 
-        async function fetchData()
+    console.log('user joined', user);
+    this.setState({participantJoined: true});
+  });
+
+  this.agoraEngine.on('user-left',async (user)=>
+  {
+    //Triggers when user leaves
+    this.setState({participantJoined: false});
+    // reloadQR();
+  });
+    
+    this.state={
+      data: '',
+      isLoading: false,
+      participantJoined:false,
+      CHANNEL_NAME:'rtc8108',
+      TOKEN: null,
+      UID: 0,
+      rtcProps:{},
+    };
+  }
+  async componentDidMount()
+  {
+    //initialize loading of QR
+   await  this.reloadQR();
+  }
+
+  generateChannelName()
+  {
+    //this function will generate a random channel name of length 7
+
+    let channelName = '';
+    let length = 7; //length of channel name
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+    channelName += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+}
+    return channelName;
+  }
+
+  async reloadQR()
+  { 
+    //generate new channel name
+    let channelName = this.generateChannelName();
+
+    //change state of loader and assigne channel name
+   this.setState({isLoading: true,CHANNEL_NAME: channelName},async ()=>
+   {
+    //once channel name is assigned enter here
+    console.log(`CHANNEELLL NNNAAMMMEEE ISSSS ${this.state.CHANNEL_NAME}`);
+    
+    //make api call to generate new token
+    await axios
+    .get(`http://localhost:3001/rtc/${this.state.CHANNEL_NAME}/audience/userAccount/${this.state.UID.toString()}`)
+    .then(res=>{
+      let  newToken = res.data;
+
+      //set new token value
+      this.setState({TOKEN: newToken['rtcToken']},()=>
       {
+        //when token is set, load QR with data
+        this.setState({data: this.state.CHANNEL_NAME +'-'+this.state.TOKEN},async ()=>
+      {
+        //assign rtcProps
+        Object.assign(this.state.rtcProps,{
+          appId: this.APP_ID, //your app id
+          channel: this.state.CHANNEL_NAME, // your agora channel
+          token: this.state.TOKEN, // use null or skip if using app in testing mode
+          role: 'audience',//role of client
+          enableScreensharing: true //always true 
+        });
+
+        console.log('obj data is ',this.state.rtcProps);
+
+        //joining the channels with the parameters defined above
+        await this.agoraEngine.join(this.APP_ID, this.state.CHANNEL_NAME, this.state.TOKEN, this.state.UID);
+
+        this.setState({isLoading:false});
+        console.log(`data is ${this.state.data}`);
+      });
+      });
       
-          await agoraEngine.join(APP_ID, CHANNEL_NAME, TOKEN, UID);
-          
-          agoraEngine.on('user-joined',async (user)=>
-          {
-            await agoraEngine.unpublish(agoraEngine.localTracks);
-  
-            console.log('user joined', user);
-            setparticipantJoined(true);
-          });
-
-          
-        setIsLoading(true);
-
-      setData(CHANNEL_NAME +'-'+TOKEN);
-      console.log(`data is ${data}`)
-    setIsLoading(false);  
     }
-      fetchData();
-      }, []);
       
-      
+      );
+   });
 
-
-    return(
-    <div>
-    {participantJoined? <div style={{display: 'flex', width: '50vw', height: '100vh'}}> 
-    <AgoraUIKit rtcProps={rtcProps} connectionData={agoraEngine.remoteUsers[0]} />
+  }
+ 
+      render(){
+    // return (<div  className='QRCodeContainer'>
+    // {this.state.participantJoined? <div style={{display: 'flex', width: '20vw', height: '100vh'}}> 
+    // <AgoraUIKit rtcProps={this.state.rtcProps} connectionData={this.agoraEngine.remoteUsers[0]} />
 
     
-    </div>:
-    <div  className='QRCodeContainer'>{isLoading?<ClipLoader/>:<QRCodeSVG className='QRCodeStyle' value={data} />}</div>
-    }
+    // </div>:
+    // <div  className='QRCodeContainer'>{this.state.isLoading?<ClipLoader/>:
     
+    // <div>
+    //   <h1>Scan the QR to start sharing screen  {this.state.CHANNEL_NAME}</h1>
+    // <QRCodeSVG className='QRCodeStyle' value={this.state.data} />
+    
+    // </div>
+    // }
+    
+    // </div>
+    // }
+    
+    // </div>);
+
+    return(<div className='QRCodeContainer'>
+<div style={{display: 'flex', width: '50vw', height: '100vh'}}> 
+     <AgoraUIKit rtcProps={this.state.rtcProps} connectionData={this.agoraEngine.remoteUsers[0]} />
+     <label>{this.state.CHANNEL_NAME}</label>
+     <QRCodeSVG className='QRCodeStyle' value={this.state.data} />
     </div>
-    );
+
+    </div>);
+    
+  }
 }
 
 export default QRcode;
